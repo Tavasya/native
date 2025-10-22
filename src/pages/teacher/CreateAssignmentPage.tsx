c4c6909e7eaab5ba7e81c6d008632f2ec3676133
Tavasya Ganpati <tavasyag@gmail.com>
2025-10-22 13:55:42 +0700

    added 5 min feature

--- src/pages/teacher/CreateAssignmentPage.tsx
+++ src/pages/teacher/CreateAssignmentPage.tsx
@@ -107,6 +107,7 @@ const CreateAssignmentPage: React.FC = () => {
     { value: "1", label: "1 minute" },
     { value: "2", label: "2 minutes" },
     { value: "3", label: "3 minutes" },
+    { value: "5", label: "5 minutes" },
   ];
 
   // Section helper functions
